22856099feb0b555a452495c6319d96cbe9f470e
Dayvis-0 <amdeyvis@gmail.com>
2024-08-05 07:28:56 -0500

    Add files via upload

---
+++ Hola mundo .cpp
@@ -0,0 +1,16 @@
+// Primer programa
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(void){
+    cout<<"Hola mundo"<<endl;  // endl y \n  sirven para un espacio
+    cout<<"Bienvenido al mundo de c++"<<endl;
+    cout<<"Deyvis atao mallqui"<<endl;
+    cout<<"Programacion en c++ "<<endl;
+    cout<<"Hola mundoooooooooooooo"<<endl; 
+    
+    system("pause");
+    return 0;
+
+}
